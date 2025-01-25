3321a16df7facc15c610d8ab2d887d8ba81107ad
William Zhang <wzhang20@yahoo.com>
2025-01-25 07:47:06 -0500

    fix observer

--- observer.cpp
+++ observer.cpp
@@ -11,11 +11,16 @@ public:
     virtual void updateCondition(std::string) = 0;
 };
 
+
+
 class TemperatureDisplay : public Observer {
     double temperature;
 public:
     TemperatureDisplay() {
         temperature = 0;
+    }
+    double getTemp() {
+        return temperature;
     }
     void updateTemperature(double temp) override {
         temperature = temp;
@@ -30,6 +35,9 @@ public:
     HumidityDisplay() {
         humidity = 0;
     }
+    double getHumidity() {
+        return humidity;
+    }
     void updateTemperature(double temp) override { }
     void updateHumidity(double humidity) override { 
         this->humidity = humidity;
@@ -39,11 +47,22 @@ public:
 
 class WeatherStation { // the subject
 public:
+    WeatherStation(double temp = 0, double humidity=0, std::string condition = "") {
+        temperature = temp;
+        this->humidity = humidity;
+        this->condition = condition;
+    }
     void subscribe(Observer* client) {
         observers.push_back(client);
+        notify_change(); // update new subscriber
     }
     void unsubscribe(Observer* client) {
         observers.remove(client); // linear time remove
+        notify_change(); // update new subscriber
+    }
+    void changeTemp(double temp) {
+        temperature = temp;
+        notify_change();
     }
     void notify_change() {
         std::list<Observer*>::iterator iterator = observers.begin();
@@ -60,3 +79,15 @@ private:
     double humidity;
     std::string condition;
 };
+
+// assumes subjects/subscribers want ALL weather data
+// alternatively, I could have inherited from an abstract (Weather station) - and just had separated subjects
+
+int main() { 
+    WeatherStation station1(10, 60, "cloudy");
+
+    TemperatureDisplay* screen = new TemperatureDisplay(); 
+    station1.subscribe(screen);
+    std::cout << "station temperature\n";
+    std::cout << screen->getTemp() << "\n";
+}
