0887e96903277e00c427617009f46c5f59f2bd59
William Zhang <wzhang20@yahoo.com>
2025-03-11 22:49:11 -0400

    add command client code

--- command.cpp
+++ command.cpp
@@ -4,7 +4,16 @@
 #include <queue>
 // Resource: https://student.cs.uwaterloo.ca/~cs446/1171/Arch_Design_Activity/Command.pdf
 
-class Chef;
+// Receives commands/orders
+class Chef {
+    public:
+        void cook() {
+            std::cout << "Cooking food!\n";
+        }
+        void pourDrink() {
+            std::cout << "Preparing drink!\n";
+        }
+};
 
 class Order {
     public:
@@ -45,18 +54,20 @@ class Waiter {
         std::queue<Order*> orders;
 };
 
-// Receives commands/orders
-class Chef {
-public:
-    void cook() {
-        std::cout << "Cooking food";
-    }
-    void pourDrink() {
-        std::cout << "pouring drink";
-    }
-};
-
 int main() {
     Waiter service;
+
+    // Create OrderFood and OrderDrink objects - they are directly connected to the chef's "menu"
+    Order* order1 = new OrderFood();
+    Order* order2 = new OrderDrink();
+
+    // Waiter takes the orders
+    service.takeOrder(order1);
+    service.takeOrder(order2);
+
+    // Waiter places the orders
+    service.placeOrders();
+    delete order1;
+    delete order2;
     return 0;
 }
